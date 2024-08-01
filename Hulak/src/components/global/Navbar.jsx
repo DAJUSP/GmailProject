b8170473505a42093f49cd16485a2207f5fdcaf3
sudipandit <112564048+dajusudeep@users.noreply.github.com>
2024-08-01 23:36:05 +0545

    project6

--- Hulak/src/components/global/Navbar.jsx
+++ Hulak/src/components/global/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { RxHamburgerMenu } from "react-icons/rx";
-import img from '../../assets/image/gmail-logo.jpg'
+import img from '../../assets/image/GmailLogo.jpg'
 
 const Navbar = () => {
   return (
@@ -10,8 +10,15 @@ const Navbar = () => {
                 <div className='p-3 rounded-full hover:bg-gray-200 cursor-pointer'>
                     <RxHamburgerMenu size={20} />
                 </div>
-                <img src={img} alt="gmail logo" />
+                <img className='w-8' src={img} alt="gmail logo" />
+                <h2 className='text-2xl font-medium text-gray-500'>Gmail</h2>
             </div>
+        </div>
+        <div className='md:block hidden w-[50%] mr-60'>
+          <div className='flex items-center bg-[#EAF1FB] px-2 py-3 rounded-full'>
+            {/* <IoIoSearch size={"20px"} className='text-gray-700'/> */}
+            <input type="text" placeholder='search' className='rounded-full w-full bg-transparent outline-none px-1' />
+          </div>
         </div>
     </div>
   )
