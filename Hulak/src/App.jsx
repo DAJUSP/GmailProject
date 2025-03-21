2b4c2d1fd3c05301569ee3e6db7fc1a19536be3c
sudipandit <112564048+dajusudeep@users.noreply.github.com>
2025-03-21 21:24:34 +0545

    init

--- Hulak/src/App.jsx
+++ Hulak/src/App.jsx
@@ -1,13 +1,13 @@
-import './App.css'
-import Navbar from './components/global/Navbar'
+import "./App.css";
+import Navbar from "./components/global/Navbar";
 
 function App() {
-
-  return (
-    <div className='bg-[#F6F8FC] h-screen w-screen overflow-hidden'>
-    <Navbar/>
-    </div>
-  )
+	return (
+		<div className="bg-[#F6F8FC] h-screen w-screen overflow-hidden">
+			<Navbar />
+			<Login />
+		</div>
+	);
 }
 
-export default App
+export default App;
