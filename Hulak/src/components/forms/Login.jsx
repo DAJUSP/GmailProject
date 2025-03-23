e8ed06ee21199ac828aca97235a1ed39dc4a0952
sudipandit <112564048+dajusudeep@users.noreply.github.com>
2025-03-23 11:44:19 +0545

    init

--- Hulak/src/components/forms/Login.jsx
+++ Hulak/src/components/forms/Login.jsx
@@ -1,20 +1,23 @@
-import React from 'react';
+import React from "react";
 
 const Login = () => {
-  return (
-    <div className="login-container">
-      <h2>Login</h2>
-      <form>
-        <label>Username:</label>
-        <input type="text" />
-        <br />
-        <label>Password:</label>
-        <input type="password" />
-        <br />
-        <button type="submit">Login</button>
-      </form>
-    </div>
-  );
+	return (
+		<div className="login-container">
+			<h2>Login</h2>
+			<form>
+				<label>Username:</label>
+				<input type="text" />
+				<br />
+				<label>Password:</label>
+				<input type="password" />
+				<br />
+				<label>Conform Password:</label>
+				<input type="password" />
+				<br />
+				<button type="submit">Login</button>
+			</form>
+		</div>
+	);
 };
 
 export default Login;
